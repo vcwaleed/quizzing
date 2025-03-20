bea32ff3e02c4612e875518a2b41df571e437385
Talha Waleed <talhawaleed460@gmail.com>
2025-03-20 07:30:04 -0700

    Create Navbar and Custom button

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,99 @@
+<script setup>
+const props = defineProps({
+  buttonType: {
+    type: String,
+    default: "primary",
+  },
+  buttonSize: {
+    type: String,
+    default: "medium",
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  action: {
+    type: Function,
+    default: () => {},
+  },
+});
+const handleClick = () => {
+  props.action();
+};
+</script>
+<template>
+  <button
+    :class="['custom-button', buttonType, buttonSize]"
+    :disabled="disabled"
+    @click="handleClick"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/variables" as *;
+
+.custom-button {
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
+  border-radius: 4px;
+  transition: background-color 0.3s, transform 0.2s;
+  display: inline-block;
+  text-align: center;
+
+  &.primary {
+    background-color: $primary-color;
+    color: $secondary-color;
+    border: 1px solid $secondary-color;
+    &:hover {
+      background-color: $secondary-color;
+      color: $primary-color;
+    }
+    &:active {
+      transform: scale(0.98);
+    }
+  }
+  &.secondary {
+    background-color: #6c757d;
+    color: white;
+    &:hover {
+      background-color: #5a6268;
+    }
+    &:active {
+      transform: scale(0.98);
+    }
+  }
+
+  &.danger {
+    background-color: #dc3545;
+    color: white;
+    &:hover {
+      background-color: #c82333;
+    }
+    &:active {
+      transform: scale(0.98);
+    }
+  }
+  &.small {
+    padding: 5px 10px;
+    font-size: 14px;
+  }
+
+  &.medium {
+    padding: 10px 20px;
+    font-size: 16px;
+  }
+  &.large {
+    padding: 13px 20px;
+    font-size: 18px;
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+}
+</style>
--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,8 +1,103 @@
 <script setup>
-</script>
-<template>
-<h1>Hi from navbar</h1>
-</template>
-<style lang="scss" scoped>
+import ButtonComponent from "./ButtonComponent.vue";
 
+const handleButtonClick = () => {
+  console.log('Button clicked via prop method!');
+};
+
+const Navbar_links = [
+  { name: 'Create Quiz', path: '/createquiz' },
+  { name: 'Give Quiz', path: '/givequiz' },
+];
+</script>
+
+<template>
+  <section class="nav_bar">
+    <div class="logo_div">
+      <img src="../assets/logo.png" width="55px" />
+      <span>Quizzy</span>
+    </div>
+    <div class="context_div">
+      <div class="link_div">
+        <ul>
+          <li v-for="(item, index) in Navbar_links" :key="index">
+            <a :href="item.path">{{ item.name }}</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ButtonComponent
+          buttonType="primary"
+          buttonSize="large"
+          :action="handleButtonClick"
+          >Login</ButtonComponent
+        >
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/variables' as *;
+
+.nav_bar {
+  display: flex;
+  justify-content: space-between;
+  padding-left: 50px;
+  padding-right: 50px;
+  
+  .logo_div {
+    display: flex;
+    align-items: center;
+    
+    span {
+      padding-left: 8px;
+      font-weight: lighter;
+      font-size: x-large;
+      font-family: sans-serif;
+      color: $title_col;
+    }
+  }
+
+  .context_div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 20px;
+
+    .link_div {
+      display: flex;
+      flex-direction: row;
+      color: $sub_text_color;
+      padding-right: 80px;
+      font-family: sans-serif;
+      font-weight: lighter;
+
+      ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        
+        li {
+          display: inline-block;
+          margin-right: 30px;  
+
+          a {
+            text-decoration: none;
+            color: inherit;  
+            font-weight: lighter;
+            transition: color 0.3s ease, background-color 0.3s ease;
+            
+            &:hover {
+              
+              color:  $secondary-color;
+              cursor: pointer;
+            
+            }
+          }
+        }
+      }
+    }
+  }
+}
 </style>
